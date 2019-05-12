42956b8e7c4dffa4252f548fa1c99105b50dabcf
necogane <necogane+bb@gmail.com>
2019-05-12 22:27:36 +0900

    no message

--- README.md
+++ README.md
@@ -1,1 +1,162 @@
-# Create-Bone-Based-OnEdge
+# Create Bone Based On Edge
+
+## Description
+
+「Create Bone Based On Edge」は、Blender 2.79b用のアドオンです。
+選択したエッジに基づいて新しいボーンを作成します。
+
+"Create Bone Based On Edge" is an add-on to Blender 2.79b.
+Creates a new bone based on the selected edge.
+
+## Download
+
+画面右上にある［Clone or download］ ＞ ［Dwonload ZIP］の順にクリックして、Zipファイルをダウンロードしてください。
+
+"Clone or download" > "Dwonload ZIP"
+
+## Install
+
+1. ダウンロードしたZipファイルを解凍します。
+1. Blenderの［ユーザー設定］画面を開きます。
+1. ［アドオン］タブにある［ファイルからアドオンをインストール］ボタンを押して、解凍したフォルダ内にある［create_bone_based_on_edge.py］ファイルを選択しインストールします。
+1. 画面左にある［サポートレベル］ > ［テスト中］を押して、テスト中のアドオンを表示します。
+1. ［Create Bone Based On Edge］の左にあるチェックをつけて、アドオンを有効にします。
+
+---
+
+1. Unzip a file.
+1. Open User Preferences.
+1. Select the Add-ons tab and press Install Addon and Select the "create_bone_based_on_edge.py" file.
+1. Press Support Level > "Testing" on the left side of the screen to display the add-on under test.
+1. Enable the "Create Bone Based On Edge" Addon.
+
+![Enable an add-on.](./2.79/document/images/20190512071249_000.png)
+
+## Usage
+
+3Dビュー > 編集モード で［辺選択］を有効にし［Ctrl + E］キーを押すと、以下の3つのメニューが追加されています。
+
+Enable "Edge select" in 3D view > Edit mode, Pressing Ctrl + E adds the following three menus.
+
+![Enable Edge select.](./2.79/document/images/20190512074514_000.png)
+
+### Create Bone From Edge
+
+選択した辺の位置に新しいボーンを作成します。
+
+Creates a new bone from the selected edge location
+
+![Creates a new bone from the selected edge location](./2.79/document/images/20190512073520_001.png)
+
+#### Demo1
+
+![Demo1](./2.79/document/images/create_bone_frome_dge_001.gif)
+
+#### Note1
+
+Ctrl + Clickや、Alt + Clickで選択した辺などは1つとしてカウントされます。Shift + Click で複数の辺を選択するようにしてください。
+
+Edges selected with Ctrl + Click or Alt + Click are counted as one. Make sure to select multiple edges with Shift + Click.
+
+### Create Bone From Edge Loops Midpoint
+
+2つ以上選択したエッジループの中点にボーンを作成します。
+
+Creates a new bone from the midpoints of two or more selected edge loops.
+
+![Create Bone From Edge Loops Midpoint](./2.79/document/images/20190512073520_002.png)
+
+#### Demo2
+
+![Demo2](./2.79/document/images/create_bone_from_edge_loops_midpoint_001.gif)
+
+### Create Bone Settings
+
+ボーンの設定ダイアログを表示します。  
+作成されるボーンの名前、親子関係の有無、ボーンの接続の有無などを変更できます。
+
+［ユーザー設定］画面 > ［アドオン］タブ > ［Create Bone Based On Edge］の［ユーザー設定:］にも同様の設定項目があります。  
+デフォルト設定を保存しておきたい場合には、［ユーザー設定］画面で［ユーザー設定の保存］を行ってください。
+
+Display the bone setting dialog.  
+You can change the bone's name, whether it is a parent-child relationship, and whether there is a bone connection.
+
+![Display the bone setting dialog](./2.79/document/images/20190512073520_003.png)
+
+![Display the bone setting dialog](./2.79/document/images/20190512204314_000.png)
+
+#### BoneName
+
+ボーンの名前を入力します。
+
+Enter the name of the bone.
+
+#### BoneNameSeparator
+
+ボーンの名前に使用する区切り文字を入力します。  
+ボーン名には自動で割り振られる連番の数字があり、ボーン名とその数字の間や、後述する接尾語との間に区切り文字を使用します。
+
+入力例：「.」「_」「-」など。
+
+Enter the delimiter used for bone names.  
+The bone names are assigned consecutive numbers, and a separator is used between the bone name and the number and suffix.
+
+example：".","_","-"... etc
+
+#### BoneNameSuffix
+
+ボーンの名前の後につけ加える文字（接尾語）を入力します。必要がなければ空のままで良いです。
+
+入力例：「L」「Left」「R」「Right」など。
+
+Enter the letter (suffix) to be added after the bone's name.  
+You can leave it empty if you do not need it.
+
+example："L","Left","R","Right"... etc
+
+#### Zeropadding
+
+ボーン名に自動で割り振られる連番の数字の桁数を入力します。  
+入力された桁数に足りない場合、数字の左側をゼロ埋めします。  
+「0」を入力した場合は「1」となりゼロ埋めを行いません。
+
+入力例：「3」の場合は「001」、「5」の場合は「00001」など。
+
+Please enter the number of digits in the number.
+If the number of digits entered is insufficient, zeros are padded to the left of the number.
+If you enter "0", "1" is not zero-padded.
+
+example："3" is "001" ,"5" is "00001"... etc
+
+#### Reverse
+
+「Create Bone From Edge」を使用時に、先頭のボーンの向きを反転します。  
+作成されたボーンの向きがおかしいときには、チェックを入れたり外したりして調整してみてください。
+
+When using "Create Bone From Edge", reverse the direction of the first bone.
+If the direction of the bone you created is incorrect, check or uncheck and adjust.
+
+#### Parent
+
+一つ前に作成されたボーンを、親ボーンをセットします。  
+ボーンに親子関係をもたせたくない場合は、チェックを外してください。
+
+Set the bone created one ago as a parent bone.  
+Turn this off if you do not want the bones to have a parent-child relationship.
+
+#### Connected
+
+ボーンを接続します。  
+ボーンを接続したくない場合は、チェックを外してください。
+
+Connect the bones.  
+If you do not want to connect the bones, please uncheck.
+
+## Licence
+
+[MIT License](./LICENCE)
+
+## Author
+
+GitHub:[Samia](https://github.com/samia_done)  
+Twitter:[@samia_done](https://twitter.com/samia_done)
---
+++ create_bone_based_on_edge.py
@@ -0,0 +1,454 @@
+"""
+Copyright (c) 2019 Samia
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+"""
+
+import bpy
+import bmesh
+import copy
+import math
+from bpy.types import Operator, AddonPreferences
+from bpy.props import *
+
+bl_info = {
+    "name": "Create Bone Based On Edge",
+    "author": "Samia",
+    "version": (1, 0),
+    "blender": (2, 79, 0),
+    "location": "3D View > Mesh Editor Mode > Ctrl + E",
+    "description": "Creates a bone based on the selected edge.",
+    "warning": "",
+    "support": "TESTING",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Rigging"
+}
+
+
+class Edge2BonePreferences(AddonPreferences):
+
+    bl_idname = __name__
+
+    bone_name = bpy.props.StringProperty(
+        name="BoneName",
+        description="Bone name",
+        default="Bone"
+    )
+
+    bone_name_junction = bpy.props.StringProperty(
+        name="BoneNameSeparator",
+        description="Bone name separator",
+        default="."
+    )
+
+    bone_name_suffix = bpy.props.StringProperty(
+        name="BoneNameSuffix",
+        description="Bone name suffix",
+        default=""
+    )
+
+    zero_padding = IntProperty(
+        name="Zeropadding",
+        description="Number of digits of bone name zero padding",
+        default=3,
+        min=0,
+        max=6
+    )
+
+    is_reverse = bpy.props.BoolProperty(
+        name="Reverse",
+        description="Change the bone order to the reverse order",
+        default=False
+    )
+
+    is_parent = bpy.props.BoolProperty(
+        name="Parent",
+        description="Set parent bone",
+        default=True
+    )
+
+    use_connect = bpy.props.BoolProperty(
+        name="Connected",
+        description="When Bone has a parent,bone's head is stuck tp the parent's tail",
+        default=True
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="Create Bone Settings")
+        layout.prop(self, "bone_name")
+        layout.prop(self, "bone_name_junction")
+        layout.prop(self, "bone_name_suffix")
+        layout.prop(self, "zero_padding")
+        layout.prop(self, "is_reverse")
+        layout.prop(self, "is_parent")
+        layout.prop(self, "use_connect")
+
+
+class Edge2BoneSettingsOperator(bpy.types.Operator):
+
+    bl_idname = "object.edge_to_borne_settings"
+    bl_label = "Create Bone Settings"
+    bl_description = "Display the bone setting dialog"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    bone_name = bpy.props.StringProperty(
+        name="BoneName",
+        description="Bone name",
+        default="Bone"
+    )
+
+    bone_name_junction = bpy.props.StringProperty(
+        name="BoneNameSeparator",
+        description="Bone name separator",
+        default="."
+    )
+
+    bone_name_suffix = bpy.props.StringProperty(
+        name="BoneNameSuffix",
+        description="Bone name suffix",
+        default=""
+    )
+
+    zero_padding = IntProperty(
+        name="Zeropadding",
+        description="Number of digits of bone name zero padding",
+        default=3,
+        min=0,
+        max=6
+    )
+
+    is_reverse = bpy.props.BoolProperty(
+        name="Reverse",
+        description="Change the bone order to the reverse order",
+        default=False
+    )
+
+    is_parent = bpy.props.BoolProperty(
+        name="Parent",
+        description="Set parent bone",
+        default=True
+    )
+
+    use_connect = bpy.props.BoolProperty(
+        name="Connected",
+        description="When Bone has a parent,bone's head is stuck tp the parent's tail",
+        default=True
+    )
+
+    @staticmethod
+    def set_user_pref(self):
+        user_preferences = bpy.context.user_preferences
+        addon_prefs = user_preferences.addons[__name__].preferences
+
+        addon_prefs.bone_name = copy.copy(self.bone_name)
+        addon_prefs.bone_name_suffix = copy.copy(self.bone_name_suffix)
+        addon_prefs.bone_name_junction = copy.copy(self.bone_name_junction)
+        addon_prefs.zero_padding = int(copy.copy(self.zero_padding))
+
+        addon_prefs.is_reverse = copy.copy(self.is_reverse)
+        addon_prefs.is_parent = copy.copy(self.is_parent)
+        addon_prefs.use_connect = copy.copy(self.use_connect)
+
+    @staticmethod
+    def get_user_pref(self):
+        user_preferences = bpy.context.user_preferences
+        addon_prefs = user_preferences.addons[__name__].preferences
+
+        self.bone_name = copy.copy(addon_prefs.bone_name)
+        self.bone_name_suffix = copy.copy(addon_prefs.bone_name_suffix)
+        self.bone_name_junction = copy.copy(addon_prefs.bone_name_junction)
+        self.zero_padding = int(copy.copy(addon_prefs.zero_padding))
+
+        self.is_reverse = copy.copy(addon_prefs.is_reverse)
+        self.is_parent = copy.copy(addon_prefs.is_parent)
+        self.use_connect = copy.copy(addon_prefs.use_connect)
+
+    def __init__(self):
+        self.get_user_pref(self)
+
+    @classmethod
+    def poll(cls, context):
+        obj = context.active_object
+        if obj and obj.type == 'MESH' and (obj.mode == 'EDIT'):
+            # Check if select_mode is 'Edge Select'.
+            if context.scene.tool_settings.mesh_select_mode[1]:
+                return True
+        return False
+
+    def invoke(self, context, event):
+        scene = context.scene
+        wm = context.window_manager
+
+        return wm.invoke_props_dialog(self)
+
+    def execute(self, context):
+        self.set_user_pref(self)
+
+        return {'FINISHED'}
+
+
+class Edge2BoneBase(bpy.types.Operator):
+
+    def __init__(self):
+        user_preferences = bpy.context.user_preferences
+        addon_prefs = user_preferences.addons[__name__].preferences
+
+        self.bone_name = copy.copy(addon_prefs.bone_name)
+        self.bone_name_junction = copy.copy(addon_prefs.bone_name_junction)
+        self.bone_name_suffix = copy.copy(addon_prefs.bone_name_suffix)
+        self.zero_padding = int(copy.copy(addon_prefs.zero_padding))
+
+        self.is_reverse = copy.copy(addon_prefs.is_reverse)
+        self.is_parent = copy.copy(addon_prefs.is_parent)
+        self.use_connect = copy.copy(addon_prefs.use_connect)
+
+        self.active = None
+        self.bm = None
+        self.location = (0, 0, 0)
+        self.new_bones = []
+
+    @staticmethod
+    def _get_distance(vector0, vector1):
+        distance = math.sqrt((vector0[0]-vector1[0])**2 +
+                             (vector0[1]-vector1[1])**2 +
+                             (vector0[2]-vector1[2])**2)
+        return distance
+
+    @staticmethod
+    def _get_select_edge_location(self, context, bm):
+        new_bones = []
+        head = None
+        tail = None
+
+        if bpy.app.version[0] >= 2 and bpy.app.version[1] >= 73:
+            bm.edges.ensure_lookup_table()
+
+        for e in bm.select_history:
+            if isinstance(e, bmesh.types.BMEdge) and e.select:
+                v0 = copy.copy(e.verts[0].co)
+                v1 = copy.copy(e.verts[1].co)
+                if(head):
+                    if self._get_distance(head, v0) > self._get_distance(head, v1):
+                        v0, v1 = v1, v0
+                    if(not self.use_connect):
+                        head = copy.copy(v0)
+                    tail = copy.copy(v1)
+                else:
+                    if(self.is_reverse):
+                        v0, v1 = v1, v0
+                    head = copy.copy(v0)
+                    tail = copy.copy(v1)
+
+                new_bones.append({"head": head, "tail": tail, "roll": 0})
+                head = tail
+
+        return new_bones
+
+    @staticmethod
+    def _get_select_edge_loops_location(self, context, bm):
+        current_local = False
+        current_cursor = copy.copy(context.scene.cursor_location)
+        edge_indexes = []
+        new_bones = []
+        head = None
+        tail = None
+
+        if bpy.app.version[0] >= 2 and bpy.app.version[1] >= 73:
+            bm.edges.ensure_lookup_table()
+
+        for select_edge in bm.select_history:
+            if isinstance(select_edge, bmesh.types.BMEdge) and select_edge.select:
+                edge_indexes.append(copy.copy(select_edge.index))
+
+        bpy.ops.mesh.select_all(action='DESELECT')
+
+        # Check if local view is enabled
+        if context.space_data.local_view:
+
+            # Disable local view
+            bpy.ops.view3d.localview()
+            current_local = True
+
+        for i in range(len(edge_indexes)):
+            for e in bm.edges:
+                if(e.index == edge_indexes[i]):
+                    e.select = True
+                    bmesh.update_edit_mesh(self.active.data, True)
+
+                    bpy.ops.mesh.loop_multi_select(ring=False)
+                    bpy.ops.view3d.snap_cursor_to_selected()
+
+                    if(i == 0):
+                        head = copy.copy(context.scene.cursor_location)
+                    else:
+                        tail = copy.copy(context.scene.cursor_location)
+                        new_bones.append({"head": head, "tail": tail, "roll": 0})
+                        head = tail
+
+                    print(head, tail)
+
+                    bpy.ops.mesh.select_all(action='DESELECT')
+
+        if current_local:
+            bpy.ops.view3d.localview()
+
+        context.scene.cursor_location = current_cursor
+
+        return new_bones
+
+    def main(self, obj):
+        current_cursor = copy.copy(bpy.context.scene.cursor_location)
+        parentBone = None
+        bone_name = self.bone_name+self.bone_name_junction
+        bone_name_suffix = ""
+
+        if (self.bone_name_suffix != ""):
+            bone_name_suffix = self.bone_name_junction+self.bone_name_suffix
+
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        bpy.ops.object.add(type='ARMATURE', enter_editmode=True, location=self.location)
+
+        for i in range(len(self.new_bones)):
+            bone = bpy.context.object.data.edit_bones.new(bone_name+str(i+1).rjust(self.zero_padding, '0')+bone_name_suffix)
+            bone.head = self.new_bones[i]['head']
+            bone.tail = self.new_bones[i]['tail']
+            bone.roll = self.new_bones[i]['roll']
+            if(parentBone):
+                bone.parent = parentBone
+                bone.use_connect = self.use_connect
+            if(self.is_parent):
+                parentBone = bone
+
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+
+        bpy.ops.view3d.snap_cursor_to_center()
+        bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+
+        bpy.context.scene.cursor_location = current_cursor
+
+    @classmethod
+    def poll(cls, context):
+        obj = context.active_object
+        if obj and obj.type == 'MESH' and (obj.mode == 'EDIT'):
+            # Check if select_mode is 'EDGE'
+            if context.scene.tool_settings.mesh_select_mode[1]:
+                return True
+        return False
+
+    def invoke(self, context, event):
+
+        self.active = context.scene.objects.active
+        self.active.update_from_editmode()
+
+        self.bm = bmesh.new()
+        self.bm = bmesh.from_edit_mesh(self.active.data)
+
+        if len(self.active.data.edges) < 1:
+            self.report({'ERROR'}, "This mesh has no edges.")
+            return
+
+    def execute(self, context):
+
+        obj = context.object
+
+        if obj.type != 'MESH':
+            raise TypeError("Active object is not a Mesh.")
+
+        if obj:
+            self.main(obj)
+
+        return {'FINISHED'}
+
+
+class Edge2BoneOperator(Edge2BoneBase):
+    bl_idname = "rigging.edge_to_borne"
+    bl_label = "Create Bone From Edge"
+    bl_description = "Creates a new bone from the selected edge location"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def invoke(self, context, event):
+
+        self.active = context.scene.objects.active
+        self.active.update_from_editmode()
+
+        self.bm = bmesh.new()
+        self.bm = bmesh.from_edit_mesh(self.active.data)
+
+        if len(self.active.data.edges) < 1:
+            self.report({'ERROR'}, "This mesh has no edges.")
+            return
+
+        self.location = self.active.location
+        self.new_bones = self._get_select_edge_location(self, context, self.bm)
+
+        # --- if none report an error and quit
+        if not self.new_bones:
+            self.report({'ERROR'}, "You must select one or more sides.")
+            return
+
+        return self.execute(context)
+
+
+class EdgeLoops2BoneOperator(Edge2BoneBase):
+    bl_idname = "rigging.edge_loop_to_borne"
+    bl_label = "Create Bone From Edge Loops Midpoint"
+    bl_description = "Creates a new bone from the midpoints of two or more selected edge loops"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def invoke(self, context, event):
+
+        self.active = context.scene.objects.active
+        self.active.update_from_editmode()
+
+        self.bm = bmesh.new()
+        self.bm = bmesh.from_edit_mesh(self.active.data)
+
+        if len(self.active.data.edges) < 1:
+            self.report({'ERROR'}, "This mesh has no edges.")
+            return
+
+        self.new_bones = self._get_select_edge_loops_location(self, context, self.bm)
+
+        # --- if none report an error and quit
+        if not self.new_bones:
+            self.report({'ERROR'}, "You must select two or more sides.")
+            return
+
+        return self.execute(context)
+
+
+classes = (
+    Edge2BonePreferences,
+    Edge2BoneSettingsOperator,
+    Edge2BoneOperator,
+    EdgeLoops2BoneOperator
+)
+
+
+def edit_mesh_edge_menu(self, context):
+    self.layout.operator_context = 'INVOKE_DEFAULT'
+    self.layout.separator()
+    self.layout.operator(Edge2BoneSettingsOperator.bl_idname, icon="PLUGIN")
+
+    self.layout.separator()
+    self.layout.operator(Edge2BoneOperator.bl_idname, icon="PLUGIN")
+    self.layout.operator(EdgeLoops2BoneOperator.bl_idname, icon="PLUGIN")
+
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.VIEW3D_MT_edit_mesh_edges.append(edit_mesh_edge_menu)
+
+
+def unregister():
+    bpy.types.VIEW3D_MT_edit_mesh_edges.remove(edit_mesh_edge_menu)
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+if __name__ == "__main__":
+    register()
